518fcb70d1c094ea49e6fbc46386eef5c0f4fffc
LuizGBR <106553072+LuizGBR@users.noreply.github.com>
2022-11-14 22:36:48 -0300

    Printing Statistics

--- UDPPingClient.py
+++ UDPPingClient.py
@@ -8,12 +8,12 @@ from statistics import mean, stdev
 server_ip = '127.0.0.1'
 server_port = 30000
 n_pings = 10
-msg = 'test'
+msg = 'Luiz'
 
 def create_packet(index, message, rtt):
     # create a packet following the documention
     id = str(index).rjust(5, '0') # 5 bytes for index
-    request_type = '0' # ping request
+    request_type = '0' # ping request byte
     timestamp = str(int(rtt / 1000000) % 10000) # 4 bytes for timestamp
     msg = message.ljust(30, '\0') # 30 bytes for message
 
@@ -22,18 +22,36 @@ def create_packet(index, message, rtt):
     return packet
 
 def response_validate(response, packet):
-	if len(response) != 40:
-		return False
+    if len(response) != 40:
+        return False
 
-	validations = [
-		packet[0:5] == response[0:5],
-		response[5:6] == '1', # pong response
-		packet[6:10] == response[6:10],
-		packet[10:40] == response[10:40]
-	]
+
+    validations = [
+        packet[0:5] == response[0:5],
+        response[5:6] == '1', # pong response
+        packet[6:10] == response[6:10],
+        packet[10:40] == response[10:40]
+    ]
 
 	# if at least one element is False, returns False
-	return all(validations)
+    return all(validations)
+
+def print_statistics(rtts, packets_sent, packets_received, total_time):
+    if packets_received > 1:
+        packet_loss = 100 - packets_received/packets_sent*100
+        min_rtt = min(rtts)
+        max_rtt = max(rtts)
+        avg_rtt = mean(rtts)
+        stdev_rtt = stdev(rtts)
+
+        print(f'\n--- {server_ip}:{server_port} ping statistics ---')
+        print(f'{str(packets_sent)} packets transmitted, {str(packets_received)} received, {packet_loss}% packet loss, time={total_time} ms')
+        print(f'rtt min/avg/max/mdev = {min_rtt:.4}/{avg_rtt:.4}/{max_rtt:.4}/{stdev_rtt:.4} ms')
+    
+    else:
+        print(f'\nThere is not enough data to calculate statistics')
+
+    return
 
 def main():
     # create an UDP socket with IPv4 configuration
@@ -44,17 +62,19 @@ def main():
     n_sent = 0
     n_received = 0
     rtts = []
-    total_time = 0
-
+    total_time = time.time_ns()
+    
     for index in range(n_pings):
         rtt = time.time_ns()
 
         packet = create_packet(index, msg, rtt)
-        client_socket.sendto(packet.encode('UTF-8'), (server_ip, server_port))
+        client_socket.sendto(packet.encode('utf-8'), (server_ip, server_port))
         n_sent += 1
 
         try:
             response = client_socket.recvfrom(2048)[0]
+            response = response.decode('utf-8')
+
             response_id = int(response[0:5])
             packet_id = int(packet[0:5])
 
@@ -69,14 +89,18 @@ def main():
             if not response_validate(response, packet):
                 print('Invalid Response')
                 continue
-            
+
+            n_received += 1
             rtts.append(rtt)
             
-            print(response)
         
         except timeout:
             print('Timed Out')
 
+    total_time = time.time_ns() - total_time
+    total_time = total_time / 1000000
+
+    print_statistics(rtts, n_sent, n_received, total_time)
 
 if __name__ == "__main__":
     main()
--- UDPPingServer.py
+++ UDPPingServer.py
@@ -1,19 +1,64 @@
 from socket import *
+import random
+import time
 
-# What's your IP address and witch port should we use?
+# fixed configurations
 recieve_host = '127.0.0.1'
 recieve_port = 30000
 
-# Create a UDP socket
-# Notice the use of SOCK_DGRAM for UDP packets
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-# Assign IP address and port number to socket
-serverSocket.bind((recieve_host, recieve_port))
+# create a UDP socket
+server_socket = socket(AF_INET, SOCK_DGRAM)
+# assign IP address and port number to socket
+server_socket.bind((recieve_host, recieve_port))
 
+
+# control settings:
+simulate_delay = True
+simulate_protocol_error = True
 
 while True:
-  message, address = serverSocket.recvfrom(2048);
-  print('Recieve: ' + message.decode())
+  try:
+    message, address = server_socket.recvfrom(2048)
 
-  serverSocket.sendto(message, address)
- 
+    message = message.decode('utf-8')
+
+    message_id = message[0:5]
+    message_control_byte = message[5:6]
+    message_timestamp = message[6:10]
+    message_content = message[10:40]
+
+
+  
+    if message_control_byte == '0':
+      message_control_byte = '1' # pong response byte
+    else:  # packet error
+      server_socket.sendto(message.encode('utf-8'), address)
+      print('ERROR: ping/pong error.')
+      continue
+  
+    # protocol error
+    if simulate_protocol_error:
+      if random.random() < 0.2:
+          pass
+      elif random.random() < 0.2:
+          message_control_byte = '13'
+          print('Simulating Ping/Pong error')
+      elif random.random() < 0.2:
+          message_timestamp = '0000'
+          print('Simulating Timestamp error')
+
+
+    # sending message
+    response = message_id + message_control_byte + message_timestamp + message_content    
+
+    # with delay
+    if simulate_delay:
+      server_socket.sendto(response.encode('utf-8'), address)
+      print(f'Message sent: {message} ')
+      time.sleep(0.2)
+    else: #without delay
+      server_socket.sendto(response.encode('utf-8'), address)
+      print(f'Message sent: {message} ')
+
+  except error:
+    print(f'Error {error}')
